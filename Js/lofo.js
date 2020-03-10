75c7613bb34f5ecc4ff19fe78b55445bfdd4ad84
Mr.Sourav Ghosh <ghsourav@github.com>
2020-03-10 14:44:15 +0530

    Logo
    Bulit With HTML,CSS Full Responsive

---
+++ Js/lofo.js
@@ -0,0 +1,9 @@
+
+
+
+function year(){
+    let time=new Date();
+    var year1= time.getFullYear();
+    document.getElementById('develop').innerHTML=`<p>&copy;${year1} All Rights Reserved by <a href="https://twitter.com/ghsourav" target="_blank">ghsourav</a></p>`;
+}
+year()
