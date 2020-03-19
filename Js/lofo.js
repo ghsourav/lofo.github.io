9b39ad253d5cce29392ae304f22b640b91100711
Mr.Sourav Ghosh <ghsourav@github.com>
2020-03-19 14:39:13 +0530

    Preloader update

--- Js/lofo.js
+++ Js/lofo.js
@@ -7,3 +7,9 @@ function year(){
     document.getElementById('develop').innerHTML=`<p>&copy;${year1} All Rights Reserved by <a href="https://twitter.com/ghsourav" target="_blank">ghsourav</a></p>`;
 }
 year()
+
+let preloader = document.getElementById('loader');
+
+function loader(){
+    preloader.style.display='none';
+}
